d4c76aef32b18f9554b22ca73c78cd9b7207e178
Harry Gueguen <harry.gueguen@gmail.com>
2022-06-22 05:59:55 +0200

    Design : episode show page

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,4 +1,6 @@
 class EpisodesController < ApplicationController
+  layout "episodes"
+  
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"], except: :show
   before_action :set_episode, only: %i[ show edit update destroy ]
 
